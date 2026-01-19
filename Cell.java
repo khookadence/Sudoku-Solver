cc45d42ab01d4cf9c66b23ae8c15c00f87ad7c10
khookadence <khoo.kadence@gmail.com>
2026-01-18 18:43:40 -0800

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -17,9 +17,6 @@ public class Cell {
 	 */
 	private int boxID;//The boxID is the box to which the cell belongs.
 	
-	//USEFUL METHODS:
-	
-	///TODO: canBe 
 	//This method returns TRUE or False depending on whether the cell has the potential to be number
 	public boolean canBe(int number){
 		if(potential[number] == true)
@@ -27,13 +24,11 @@ public class Cell {
 		return false;
 	}
 	
-	///TODO: cantBe
 	//This sets the potential array to be false for a given number
 	public void cantBe(int number){
 		potential[number] = false;
 	}
 	
-	///TODO: numberOfPotentials
 	//This method returns a count of the number of potential numbers that the cell could be.
 	public int numberOfPotentials(){
 		int count = 0;
@@ -46,7 +41,6 @@ public class Cell {
 		
 	}
 	
-	///TODO: getFirstPotential
 	//This method will return the first number that a cell can possibly be.
 	public int getFirstPotential(){
 		for(int x = 0; x < 10; x ++) {
@@ -89,7 +83,6 @@ public class Cell {
 		return number;
 	}
 	
-	///TODO: setNumber
 	// This method sets the number for the cell but also sets all of the potentials for the cell (except for the solved number)
 	//		to be false
 	public void setNumber(int number) {
@@ -128,3 +121,4 @@ public class Cell {
 
 //have an object called cell
 //has an int value and a potential array
+
